cf0abcbf8454afe3947ec0130af8d53314cf17ef
Vinicius Gabriel Oliveira De Sousa <t-vinicius.sousa@cni.com.br>
2025-01-10 16:58:46 -0300

    :contruction: contruction of page Create Acount

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -2,8 +2,18 @@ import styles from '../styles/CreateAccount.module.css';
 
 function CreateAccount() {
     return (
-        <div className={styles.container}>
-            <h1>hello</h1>
+        
+        <div className={styles.container_form}>
+            <h1>Personal Information</h1>
+            <div className={styles.form_background}>
+                <input type='text' placeholder='complete name'></input>
+                <input type='email' placeholder='e-mail'></input>
+                <input type='password' placeholder='password'></input>
+                <input type='password' placeholder='confirm password'></input>
+                <input type='date'></input> 
+                <input type='text' placeholder='nationality'></input> 
+                <input type='tel' placeholder='phone'></input>
+            </div>
         </div>
     );
 }
