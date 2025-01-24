4693cc97ebc61a3126dc68ca1d31d86f7ce68bef
Vinicius Gabriel <viniganrielsoa@gmail.com>
2025-01-24 11:25:46 -0300

    :contruction: Estiization of pages, realtionais a CreationAccount.

--- src/pages/createAccount/CreateAccount.jsx
+++ src/pages/createAccount/CreateAccount.jsx
@@ -5,6 +5,7 @@ import { personalSchema, documentalSchema, addressSchema } from './validationSch
 import PersonalInformation from './PesonalInformation'
 import DocumentalInformation from './DocumentalInformation'
 import AddressInformation from './AddressInformation';
+import styles from '../../styles/CreateAccount.module.css'
 
 function CreateAccount () {
     const [step, setStep] = useState(1);
@@ -43,25 +44,26 @@ function CreateAccount () {
     }
     
     return(
-        
-        <form onSubmit={handleSubmit(step === 3 ? onSubmit : nextStep)}>
-            {step === 1 && (
-            <PersonalInformation register={register} errors={errors} nextStep={nextStep} />
-        )}
+        <div className={styles.background}>
+            <form className={styles.container_form} onSubmit={handleSubmit(step === 3 ? onSubmit : nextStep)}>
+                {step === 1 && (
+                <PersonalInformation register={register} errors={errors} nextStep={nextStep} />
+            )}
 
-        {step === 2 && (
-            <DocumentalInformation register={register} errors={errors} nextStep={nextStep} prevStep={prevStep} />
-        )}
+            {step === 2 && (
+                <DocumentalInformation register={register} errors={errors} nextStep={nextStep} prevStep={prevStep} />
+            )}
 
-        {step === 3 && (
-            <AddressInformation register={register} errors={errors} prevStep={prevStep} />
-        )}
+            {step === 3 && (
+                <AddressInformation register={register} errors={errors} prevStep={prevStep} />
+            )}
 
-        <div>
-            {step > 1 && <button type="button" onClick={prevStep}>Prev</button>}
-            <button type="submit">{step === 3 ? "Submit" : "Next"}</button>
-            </div>
-        </form>
+                <div className={styles.button_container}>
+                    {step > 1 && <button type="button" onClick={prevStep}>Prev</button>}
+                    <button type="submit">{step === 3 ? "Submit" : "Next"}</button>
+                </div>
+            </form>
+        </div>
     )
 }
 
